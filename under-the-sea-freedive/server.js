b0239db8725dbd2564595a8824647e9643cfc9b8
berryyoger <berry.yoger@gamil.com>
2025-09-09 18:45:06 +0900

    test
    test

--- under-the-sea-freedive/server.js
+++ under-the-sea-freedive/server.js
@@ -51,3 +51,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🌊 Under the Sea server running at http://localhost:${PORT}`));
+
+//test 주석
