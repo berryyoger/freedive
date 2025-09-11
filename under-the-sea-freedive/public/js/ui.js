314c8e83c984f41f82aba6f54c32b9015c4619d9
berryyoger <berry.yoger@gmail.com>
2025-09-11 14:36:54 +0900

    modified footer
    and need more

--- under-the-sea-freedive/public/js/ui.js
+++ under-the-sea-freedive/public/js/ui.js
@@ -63,14 +63,14 @@ function header(){
 function footer(){
   const f= document.createElement('footer');
   f.innerHTML = `
-            <div class="footer__pages">
-                <ul class="footer__nav">
+            <div class="footer_pages">
+                <ul class="footerNav">
                     <li class="footer__nav__item"><a href="http://eos.aidainternational.org">EOS 2.0</a></li>
                     <li class="footer__nav__item"><a href="/CodeOfConduct">Code of Conduct</a></li>
                     <li class="footer__nav__item"><a href="/Documents">Documents</a></li>
                     <li class="footer__nav__item"><a href="/Disclaimer">Disclaimer</a></li>
                 </ul>
-                <ul class="footer__nav">
+                <ul class="footerNav">
                     <li class="footer__nav__item"><a href="https://www.aidainternational.org/Athletes">Athletes</a>
                     </li>
                     <li class="footer__nav__item"><a href="https://www.aidainternational.org/Events/EventCalendar">Event
@@ -82,68 +82,49 @@ function footer(){
                     <li class="footer__nav__item"><a href="https://www.aidainternational.org/WorldRecords">World
                             Records</a></li>
                 </ul>
-                <ul class="footer__nav">
+                <ul class="footerNav">
                     <li class="footer__nav__item"><a href="/About">About</a></li>
                     <li class="footer__nav__item"><a href="/BoardAndMembers">Structure & contacts</a></li>
-<!--                    <li class="footer__nav__item"><a href="/AIDACommittees">AIDA Committees</a></li>-->
                     <li class="footer__nav__item"><a href="/Freediving">Freediving</a></li>
                     <li class="footer__nav__item"><a href="/Competitive">Competitive</a></li>
                 </ul>
-                <!-- <ul class="footer__nav">
-                    <li class="footer__nav__item"><a href="/Contact">Contact</a></li>
-                </ul> -->
             </div>
-            <div class="footer__connect add__multiple" style="overflow: visible">
-
-                <div class="domain1" id="div1">
-                    <ul class="footer__nav" style="width: auto">
-                        <li class="footer__nav__item anim1"><a href="https://www.facebook.com/aidafreediving/" target="_blank"><span class="short">AIDA official</span> FB Page</a></li>
-                        <li class="footer__nav__item anim2"><a href="https://www.facebook.com/groups/aida.freediving/" target="_blank"><span class="short">AIDA official</span> Public Group</a></li>
-                        <li class="footer__nav__item anim3"><a href="https://www.facebook.com/groups/AIDAJudges/" target="_blank"><span class="short">AIDA</span> Judges <span class="short">closed</span> Group</a></li>
-                        <li class="footer__nav__item anim4"><a href="https://www.facebook.com/groups/1477868842543526/" target="_blank"><span class="short">AIDA</span> Instructors <span class="short">Closed</span> Group</a></li>
-                    </ul>
-                </div>
+            <div class="footerCwuNav" style="overflow: visible">
                 <h2 class="footer__connect__title u-spc-bottom--sml">Connect with us</h2>
 
 
                 <ul class="smm u-spc-bottom--sml ">
 
 
-                    <li class="smm__item ">
+                    <li class="smm__item" style="text-align: left;">
                         <a id="facebook" class="smm__link">
-                            <img src="/assets/temp/eos/icons/social-facebook.svg" alt="AIDA on Facebook" >
+                            <img src="/assets/temp/eos/icons/social-facebook.svg" alt="Under the Sea on Facebook" >
                         </a>
                     </li>
 
-                    <li class="smm__item">
-                        <a href="https://www.instagram.com/aidafreediving/" class="smm__link" target="_blank">
-                            <img src="/assets/temp/eos/icons/social-instagram.svg" alt="AIDA on Twitter" >
+                    <li class="smm__item" style="text-align: left;">
+                        <a href="https://www.instagram.com/{}" class="smm__link" target="_blank">
+                            <img src="/assets/temp/eos/icons/social-instagram.svg" alt="Under the Sea on Instagram" >
                         </a>
                     </li>
 
-                    <li class="smm__item">
-                        <a href="https://www.linkedin.com/company/3741895" class="smm__link" target="_blank">
-                            <img src="/assets/temp/eos/icons/social-linkedin.svg" style="color :blue" alt="AIDA on Twitter" >
-                        </a>
-                    </li>
-
-                    <li class="smm__item">
-                        <a href="https://twitter.com/aidaint" target="_blank" class="smm__link">
-                            <img src="/assets/temp/eos/icons/social-twitter.svg" alt="AIDA on Twitter" >
+                    <li class="smm__item" style="text-align: left;">
+                        <a href="https://twitter.com/{}" target="_blank" class="smm__link">
+                            <img src="/assets/temp/eos/icons/social-twitter.svg" alt="Under the Sea on Twitter" >
                         </a>
                     </li>
 
 
-                    <li class="smm__item">
-                        <a href="https://www.youtube.com/c/aidafreediving" target="_blank" class="smm__link">
-                            <img src="/assets/temp/eos/icons/social-youtube.svg" alt="AIDA on Twitter" >
+                    <li class="smm__item" style="text-align: left;">
+                        <a href="https://www.youtube.com/c/{}" target="_blank" class="smm__link">
+                            <img src="/assets/temp/eos/icons/social-youtube.svg" alt="Under the Sea on Youtube" >
                         </a>
                     </li>
 
 
 
                 </ul>
-                <p class="u-type--primary u-type--zeta">©&thinsp;2025&thinsp;—&thinsp;AIDA International</p>
+                <p class="u-type--primary u-type--zeta">©&thinsp;2025&thinsp;—&thinsp;Under the Sea</p>
                             </div>
                         </div>
                     </div>
